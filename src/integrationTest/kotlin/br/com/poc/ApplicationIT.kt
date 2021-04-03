493b1aa68d9ed0955e6be2865021a986ce252f25
Alison Yutaka Inoue <alison.inoue@gmail.com>
2021-04-03 15:52:43 -0300

    feat: configure integration test to run kafka test containers and mock schema registry

--- src/integrationTest/kotlin/br/com/poc/ApplicationIT.kt
+++ src/integrationTest/kotlin/br/com/poc/ApplicationIT.kt
@@ -2,12 +2,15 @@ package br.com.poc
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.testcontainers.containers.KafkaContainer
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
 import javax.inject.Inject
 
 
@@ -15,6 +18,9 @@ import javax.inject.Inject
 @MicronautTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 open class ApplicationIT {
+
+    private val logger = KotlinLogging.logger {}
+
     companion object {
         @Container
         val container = PostgreSQLContainer<Nothing>("postgres:12").apply {
@@ -27,6 +33,14 @@ open class ApplicationIT {
             System.setProperty("datasources.default.password", this.password)
             System.setProperty("datasources.default.driverClassName", this.driverClassName)
         }
+
+        @Container
+        val kafka = KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.1.0")).apply {
+            withEmbeddedZookeeper()
+            start()
+            System.setProperty("kafka.bootstrap.servers", this.bootstrapServers)
+            System.setProperty("kafka.schema.registry.url", "mock://localhost:8081")
+        }
     }
 
     @Inject
@@ -34,6 +48,7 @@ open class ApplicationIT {
 
     @Test
     fun `should call endpoint`() = runBlocking {
+        logger.info { "Kafka Docker is running - ${kafka.isRunning}" }
         Assertions.assertEquals(
             "Person created!",
             personClient.createPerson(
